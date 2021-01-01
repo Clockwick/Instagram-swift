08c445f75c0d4dc2f4c5b3bb5b3a6f4f7271b1cb
arthris <oclockth@gmail.com>
2021-01-01 21:18:23 +0700

    LoginView

---
+++ Resources/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  Instagram
+//
+//  Created by Paratthakorn Sribunyong on 1/1/2564 BE.
+//
+
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+
+
+}
